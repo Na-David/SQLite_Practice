e18c5cf5bd772d9a1c295ce609f83abb49d48978
Aaron NA <hna206@naver.com>
2021-08-24 11:32:48 +1200

    Add project files.

---
+++ SQLite_Demo/DatabaseManager.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLite_Demo
+{
+    public static class DatabaseManager
+    {
+        //YOUR CLASS NAME MUST BE YOUR TABLE NAME
+        public static SQLiteConnection db;
+        public static string databasePath;
+        public static string databaseName;
+
+        static DatabaseManager()
+        {//Using Xamarin Essentials to set the path. AppdataDirectory gets backed up by Android
+            databaseName = "ToDoList.db";
+            databasePath = Path.Combine(Xamarin.Essentials.FileSystem.AppDataDirectory, databaseName);
+            //  if (databasePath != null)
+            //  {
+            db = new SQLiteConnection(databasePath);
+            //   }
+            // Initializes a new instance of the Database. if the database doesn't exist, it will create the database and all the tables. This is important even if you have a table a table
+            db.CreateTable<tblToDo>();
+        }
+
+    }
+}
---
+++ SQLite_Demo/Model/tbToDo.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+
+
+namespace SQLite_Demo.Model
+{
+    class tbToDo
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime Current_Date { get; set; }
+
+    }
+}
